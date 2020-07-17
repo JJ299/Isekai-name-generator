de75621f2378aeb33827596f532075a1e5672c8a
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-17 12:05:28 -0400

    Updated some CSS/HTML. Added JS where it gives user the choice of setting. Will finalize later on

--- generator.js
+++ generator.js
@@ -1,1 +1,11 @@
-console.log("Hello this is a test");
+const setting = prompt("Pick one setting where you want to get Isekai'd into: Fantasy or historical.");
+
+if(setting.toUpperCase() === "FANTASY" || setting.toUpperCase() === "HISTORICAL" ) {
+	const isekai = "<p>You picked the right choice</p>"
+	document.querySelector('p').innerHTML = isekai;
+} else {
+	const isekai = "<p>That was not part of the choice</p>"
+	document.querySelector('p').innerHTML = isekai;
+}
+
+	
