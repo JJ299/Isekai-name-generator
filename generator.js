ca8a48e06d68b9d0f6e38bb0472a4a44611fc93b
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-18 11:13:37 -0400

    Updated/added feature where the choices will diverge

--- generator.js
+++ generator.js
@@ -1,11 +1,16 @@
 const setting = prompt("Pick one setting where you want to get Isekai'd into: Fantasy or historical.");
 
-if(setting.toUpperCase() === "FANTASY" || setting.toUpperCase() === "HISTORICAL" ) {
-	const isekai = "<p>You picked the right choice</p>"
-	document.querySelector('p').innerHTML = isekai;
-} else {
-	const isekai = "<p>That was not part of the choice</p>"
-	document.querySelector('p').innerHTML = isekai;
-}
+	if(setting.toUpperCase() === "FANTASY") {
+		const isekai = "<p>Here's a fantasy title</p>"
+		document.querySelector('p').innerHTML = isekai;
+
+	} else if(setting.toUpperCase() === "HISTORICAL") {
+		const isekai = "<p>Here's a historical title</p>"
+		document.querySelector('p').innerHTML = isekai;
+
+	} else {
+		const isekai = "<p>That was not part of the choice</p>"
+		document.querySelector('p').innerHTML = isekai;
+	}
 
 	
